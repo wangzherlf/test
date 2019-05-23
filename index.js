4319a1941fd763ce0b1c0aacb9ca0b5deab92314
悟 <wangzherlf>
2019-05-23 23:34:17 +0800

    edit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 ;(function(){
 	console.log('mac_dev_branch')
 	console.log('tencent-add')
-	console.log('index1')
+	console.log('indexiqqqqqqqqqqqq1')
 	console.log('tencent-end')
 	console.log('index')
 }())
